4382e197c0523e99f2df9ec98f05a7e89d817462
Hasan Çağrı Bozaklı <facewolf866@hotmail.com>
2023-06-16 10:54:38 +0300

    .
    .

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import store from "./store";
 import CountryFlag from "vue-country-flag-next";
 import "@/assets/css/reset.css";
 
-import "bootstrap-vue/dist/bootstrap-vue.css";
 import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
 createApp(App).use(store).use(router).use(CountryFlag).mount("#app");
--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true,
+    transpileDependencies: true,
 
-  css: {
-    extract: false
-  }
+    css: {
+        extract: false,
+    },
 });
